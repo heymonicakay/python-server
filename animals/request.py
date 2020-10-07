02d41382306f34826da88f0930c411c27b932430
heymonicakay <hey.monicakay@gmail.com>
2020-10-07 12:07:02 -0500

    first commit

---
+++ animals/request.py
@@ -0,0 +1,27 @@
+ANIMALS = [
+    {
+      "id": 1,
+      "name": "Doodles",
+      "breed": "Poodle",
+      "customerId": 1,
+      "locationId": 2
+    },
+    {
+      "id": 2,
+      "name": "Kaos",
+      "breed": "Aussie-Shep",
+      "customerId": 2,
+      "locationId": 1
+    },
+    {
+      "id": 3,
+      "name": "Rufus",
+      "breed": "Basset",
+      "customerId": 3,
+      "locationId": 2
+    }
+]
+
+
+def get_all_animals():
+    return ANIMALS
---
+++ employees/request.py
@@ -0,0 +1,42 @@
+EMPLOYEES = [
+    {
+      "id": 1,
+      "name": "Mario Kart",
+      "animalId": 2,
+      "locationId": 1
+    },
+    {
+      "id": 2,
+      "name": "Mark Ruffullo",
+      "animalId": 2,
+      "locationId": 1
+    },
+    {
+      "id": 3,
+      "name": "Luke Bryan",
+      "locationId": 1,
+      "animalId": 2
+    },
+    {
+      "name": " Sarah Anderson",
+      "locationId": 1,
+      "animalId": 2,
+      "id": 4
+    },
+    {
+      "name": "Sally Fields",
+      "locationId": 2,
+      "animalId": 3,
+      "id": 5
+    },
+    {
+      "name": "Bill Hillsman",
+      "locationId": 2,
+      "animalId": 3,
+      "id": 6
+    }
+]
+
+
+def get_all_employees():
+    return EMPLOYEES
